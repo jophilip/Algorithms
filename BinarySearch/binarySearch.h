9d74abe9c20bb5f0debb383ad48532a6630b750e
Philip Joseph <65232161+jophilip@users.noreply.github.com>
2020-05-19 13:27:57 +0200

    Create binarySearch.h

---
+++ BinarySearch/binarySearch.h
@@ -0,0 +1,8 @@
+#ifndef BINARYSEARCH_H_
+#define BINARYSEARCH_H_
+
+int iterative_binarySearch(int A[], int n, int T);
+int recursive_binarySearch(int A[], int left, int right, int T );
+int test_binarySearch();
+
+#endif
