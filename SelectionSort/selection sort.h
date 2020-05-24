5394da0c44863c568b2b25cdc5075c2ca48360a3
Philip Joseph <65232161+jophilip@users.noreply.github.com>
2020-05-24 11:23:20 +0200

    Create selection sort.h

---
+++ SelectionSort/selection sort.h
@@ -0,0 +1,10 @@
+#ifndef _SELECTIONSORT_H_
+#define _SELECTIONSORT_H_
+
+
+void swap(int *elem1, int *elem2);
+void selectionSort(int arr[], int n);
+void print(int arr[], int size);
+int test_main();
+
+#endif
